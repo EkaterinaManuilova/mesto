d8883cb4c66f95f1c91436e1fd98f0470359ccc1
EkaterinaManuilova <e.manuilova@autosyst.ru>
2021-12-21 19:00:25 +0300

    initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,45 @@ let profileUserJob = document.querySelector('.profile__userjob');
 let nameInput = formElement.querySelector('.form__input_name_username');
 let jobInput = formElement.querySelector('.form__input_name_profession');
 
-function editForm() {
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+const editForm = () =>  {
     editPopup.classList.add('popup_opened');
     nameInput.value = profileUserName.textContent;
     jobInput.value = profileUserJob.textContent;
 }
 
-function editFormClose() {
+const editFormClose = () => {
     editPopup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+const formSubmitHandler = evt => {
 	evt.preventDefault(); 
 	
 	profileUserName.textContent = nameInput.value;
@@ -25,6 +53,40 @@ function formSubmitHandler(evt) {
 
     editFormClose();
 }
+
+function addCard(nameValue, linkValue) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    const cardElementTitle = cardElement.querySelector('.element__title');
+    const cardElementImage = cardElement.querySelector('.element__image');
+    const cardElementLike = cardElement.querySelector('.element__like');
+    const cardElementTrash = cardElement.querySelector('.element__trash');
+
+    cardElementTitle.textContent = nameValue;
+    cardElementImage.textContent = linkValue;
+    cardElementImage.setAttribute('src', linkValue);
+    cardElementImage.setAttribute('alt', nameValue);
+
+    cardElement.addEventListener('click', () =>{
+        imagePopup.classList.add('popup_opened');
+        linkImage.setAttribute('src', linkValue);
+        titleImage.textContent = nameValue;
+    });
+
+    cardElementLike.addEventListener('click', event => {
+        event.target.classList.toggle('element__like_active');
+    });
+
+    cardElementTrash.addEventListener('click',  () => {
+        cardElementTrash.parentElement.remove();
+    });
+
+    cardsContainer.prepend(cardElement);
+}
+
+initialCards.forEach(function(element){
+    addCard(element.name, element.link);
+});
 
 editButton.addEventListener('click', editForm);
 editCloseButton.addEventListener('click', editFormClose);
