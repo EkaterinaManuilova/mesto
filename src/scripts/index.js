d1808311bb45ac441e47e1a68bfed2164e69987d
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-02-22 17:27:05 +0300

    src, Section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
-import {Card} from  './Card.js';
-import {FormValidator} from './FormValidator.js';
+import {Card} from  './components/Card.js';
+import {FormValidator} from './components/FormValidator.js';
 import {editButton, editPopup, profileForm, profileUserName, profileUserJob, nameInput,
     jobInput, cardsContainer, addButton, addPopup, addForm, addFormTitle, addFormLink,
     imagePopup, linkImage, titleImage, popups, formValidators, initialCards, validationProps} from './utils/constants.js';
